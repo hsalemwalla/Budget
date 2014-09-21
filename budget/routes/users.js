3231b1f1a1e570345aaf1de16ccf1cfc41e487db
hamritsidhu <hamritsidhu_94@hotmail.com>
2014-09-21 01:05:13 -0400

    minor change

--- budget/routes/users.js
+++ budget/routes/users.js
@@ -3,14 +3,13 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/userPoints', function(req, res) {
-    console.log('req data points: ');
-    console.log(req.body.data);
+    res.send(GetUserPoints());
     res.sendStatus(200);
 });
 
-router.get('/question', function(req, res) {
-    console.log('req data question: ');
-    console.log(req.body.data);
+router.get('/question', function (req, res) {
+    console.log("data:" + req.body.data);
+   // res.send(PromptAnswer())
     res.sendStatus(200);
 });
 
