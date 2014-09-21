f0ff01ecbe8833bd21e9217a9cde4b52eff93e63
hsalemwalla <hsalemwalla@gmail.com>
2014-09-21 00:26:25 -0400

    Fixed Gets and Post calls work

--- budget/public/javascripts/scripts.js
+++ budget/public/javascripts/scripts.js
@@ -1,15 +1,74 @@
-sendRequest(
-    'users/test', 
-    { data: JSON.stringify({ intent: 'test Intent'})}
-);
 
 
-
-function sendRequest(url, data) {
+function fetchRequest(url, data) {
     $.ajax({
         url: url,
-        type: 'PUT',
+        type: 'GET',
         data: data,
-        dataType: 'json'
+        dataType: 'json',
+        success: successfulFetch
     });
 }
+
+function postAnswer (url, data) {
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+        success: successfulPostAnswer
+    });
+}
+
+function postPoints (url, data) {
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+        success: successfulPostPoints
+    });
+}
+
+
+function successfulFetch(oData) {
+    console.log(oData.body.data);
+    if (/* question */ true) {
+        getAnswers(/* question */);
+    } else {
+
+    }
+}
+
+function successfulPostAnswer(oData) {
+    console.log(oData.body.data);
+}
+
+function successfulPostPoints(oData) {
+    console.log(oData.body.data);
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *function sendRequest(url, data) {
+ *    $.ajax({
+ *        url: url,
+ *        type: 'PUT',
+ *        data: data,
+ *        dataType: 'json',
+ *        success: successfulRequest
+ *    });
+ *}
+ *function successfulRequest(oData) {
+ *    console.log(oData.body.data);
+ *}
+ */
--- budget/routes/users.js
+++ budget/routes/users.js
@@ -2,9 +2,28 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.put('/test', function(req, res) {
-    console.log('req data: \n');
+router.get('/userPoints', function(req, res) {
+    console.log('req data points: ');
     console.log(req.body.data);
+    res.sendStatus(200);
+});
+
+router.get('/question', function(req, res) {
+    console.log('req data question: ');
+    console.log(req.body.data);
+    res.sendStatus(200);
+});
+
+router.get('/answers', function(req, res) {
+    console.log('req data answers: ');
+    console.log(req.body.data);
+    res.sendStatus(200);
+});
+
+router.post('/postAnswer', function(req, res) {
+    console.log('req data: ');
+    console.log(req.body.data);
+    res.status(200).end();
 });
 
 module.exports = router;
